35a55a32967f2eb164b0087b57824cbcf192d4f7
Zhymabek Roman <61125068+ZhymabekRoman@users.noreply.github.com>
2023-04-11 10:43:25 +0600

    update: fix messy digit string implementation
    TODO: requiere refactor

--- kz_iin_validator/utils.py
+++ kz_iin_validator/utils.py
@@ -3,8 +3,13 @@ import re
 DIGIT_STRING = re.compile(r"^\d+$")
 
 
-def is_digit_string(input_string: str, fast: bool = True):
-    if fast:
+def is_digit_string(input_string: str, fast: bool = True, weak_fast_check: bool = False) -> bool:
+    if fast and weak_fast_check:
+        raise ValueError("weak_fast_check and fast can't be specified at one time")
+     
+    if weak_fast_check:
         return bool(DIGIT_STRING.match(input_string))
+    elif fast:
+        return input_string.isdigit()
     else:
         return all(char.isdigit() for char in input_string)

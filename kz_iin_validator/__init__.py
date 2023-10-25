75602a6a523b8e113140811203a7f73e103e2bf9
ZhymabekRoman <robanokssamit@yandex.ru>
2023-10-25 21:03:41 +0600

    Bump version

--- kz_iin_validator/__init__.py
+++ kz_iin_validator/__init__.py
@@ -3,4 +3,4 @@ from .iin_generator import generate_iin
 from .iin_validator import IIN, ValidatedIIN, safe_validate_iin, validate_iin
 from .iin_extractor import extract_iin
 
-__version__ = "0.7.0"
+__version__ = "0.7.1"
